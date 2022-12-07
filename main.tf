4ebbcb3ee830b3a85c30879548ebedb134738836
Bryant Biggs <bryantbiggs@gmail.com>
2022-12-07 09:19:20 -0500

    feat!: Refactor to include origin access controls as well as origin access identities

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
-locals {
-  create_origin_access_identity = var.create_origin_access_identity && length(keys(var.origin_access_identities)) > 0
-}
-
 resource "aws_cloudfront_origin_access_identity" "this" {
-  for_each = local.create_origin_access_identity ? var.origin_access_identities : {}
+  for_each = { for k, v in var.origin_access_identities : k => v if var.create_origin_access_identity }
 
   comment = each.value
 
